41c9de2a4940d9aecb9d841fc489b84b0b1401eb
fabianoLira <fabianolira@cge.pi.gov.br>
2023-01-23 12:03:41 -0300

    primeiro commit

---
+++ src/componentes/Banner/Banner.js
@@ -0,0 +1,11 @@
+import './Banner.css'
+
+function Banner() {
+    return (
+        <header className='banner'>
+            <img src="/imagens/1.jpeg" alt="banner principal da pagina" />
+        </header>
+    )
+}
+
+export default Banner
